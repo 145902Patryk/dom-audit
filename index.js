ff1d6fec4396e8875fd1c74b1884ccdf3a683ec6
145902Patryk <patryklaszuk98@gmail.com>
2023-08-25 11:08:38 +0200

    functions `maxDepth`, `maxChildren`

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-exports.countDOM = function () {
-    let count = document.getElementsByTagName('*').length
-    console.log(`${count} elements found.`);
+const elementDepth = (el) => {
+    let depth = 0
+    while (null !== el.parentElement) {
+        el = el.parentElement
+        depth++
+    }
+    return depth
+}
+
+exports.countDOM = () => {
+    return document.getElementsByTagName('*').length;
+}
+exports.maxDepth = () => {
+    const depths = []
+    Array.from(document.querySelectorAll("*")).forEach(node => {
+        depths.push(elementDepth(node));
+    });
+    return Math.max(...depths)
+}
+exports.maxChildren = (excluded=['head']) => {
+    const children = []
+    Array.from(document.querySelectorAll("*")).filter(node =>
+        !node.closest(...excluded)
+    ).forEach(node => {
+        children.push(node.childElementCount);
+    });
+    return Math.max(...children)
 }
